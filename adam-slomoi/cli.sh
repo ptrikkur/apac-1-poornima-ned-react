04268bf33b5d8324dcc92127131c558636b6c6ed
aslomoi <aslom001@gmail.com>
2021-05-19 16:26:08 +1000

    fixing -p

--- adam-slomoi/cli.sh
+++ adam-slomoi/cli.sh
@@ -1,7 +1,7 @@
 touch cli-output.txt
 echo "Describe how PATH works?" > cli-output.txt
 echo "It is the location/s in which the binaries for commands are stored and used to execute commands" >> cli-output.txt
-mkdir archive
+mkdir -p archive
 cp cli-output.txt archive/how-path-works.txt
 echo "What command shows us previously run commands?" > cli-output.txt
 echo "history" >> cli-output.txt
