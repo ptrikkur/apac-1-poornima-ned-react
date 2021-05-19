d59eba65bb76d122ecfcab813a8f3e6a2863ae4c
Ronald Chan <ronald.chan@ibm.com>
2021-05-19 13:56:06 +0800

    Completes assignment

---
+++ ronald-chan/cli.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# touch cli-output.txt
+echo "Describe how PATH works?" > cli-output.txt
+mkdir archive
+cp cli-output.txt archive/how-path-works.txt
+echo "What command shows us previously run commands?" > cli-output.txt
+cp cli-output.txt archive/previously-run-commands-command.txt
+echo "Script run successful."
