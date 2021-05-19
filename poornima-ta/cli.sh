34e23f00e50dbdefd695dbf0ec40c1008cf013a4
ptrikkur <ptrikkur@in.ibm.com>
2021-05-19 13:17:06 +0530

    removed a file

--- poornima-ta/cli.sh
+++
@@ -1,9 +0,0 @@
-touch cli-output.txt
-echo 'Describe how PATH works?' > cli-output.txt
-echo 'Path Lists directories of all binaries' >> cli-output.txt
-mkdir archive
-cp cli-output.txt archive/how-path-works.txt
-echo 'What command shows us previously run commands?' > cli-output.txt
-echo 'history' >> cli-output.txt
-cp cli-output.txt archive/previously-run-commands-command.txt
-echo 'Script run successful.'
