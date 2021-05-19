3ed90e52c1d708bf97675d1ca42c0cc9183ea08b
Kisoong Jang <kisjang@kr.ibm.com>
2021-05-19 14:57:46 +0900

    cli-assignment

---
+++ kisoong-jang/day-1/cli.sh
@@ -0,0 +1,8 @@
+
+
+echo "When executing a commang, finds and executes the command in the path" > cli-output.txt
+mkdir archive
+cp cli-output.txt ./archive/how-path-works.txt
+echo "history" > cli-output.txt
+cp cli-output.txt ./archive/previously-run-commands-command.txt
+echo "Script runt successful"
